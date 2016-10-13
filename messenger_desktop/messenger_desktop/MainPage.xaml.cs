46e8e162a71c89aba93dac45dddab8b9e28cf181
Mbladra <mdrabla1@gmail.com>
2016-10-13 02:07:56 -0400

    Windows 10 wrapper for messenger.com/login written in C#

---
+++ messenger_desktop/messenger_desktop/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace messenger_desktop
+{
+    /// <summary>
+    /// navigates to messenger.com/login
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            messengerWeb.Navigate(new Uri("https://www.messenger.com/login"));
+        }
+    }
+}
