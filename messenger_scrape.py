47699d83062a782f4815d2c758e78bec892328c2
Malik <mdrabla1@gmail.com>
2017-01-03 16:08:09 -0600

    Update messenger_scrape.py

--- messenger_scrape.py
+++ messenger_scrape.py
@@ -2,26 +2,26 @@ from bs4 import BeautifulSoup
 import urllib
 
 def get_soup(url):
-    raw_page = urllib.urlopen(url).read()
-    soup = BeautifulSoup(raw_page, "html.parser")
-    return soup
+  	  raw_page = urllib.urlopen(url).read()
+  	  soup = BeautifulSoup(raw_page, "html.parser")
+   	 return soup
 
 def get_text_url(id):
-    url = 'http://www.messenger.com/t/'
-    url = url + str(id)
-    return url
+  	  url = 'http://www.messenger.com/t/'
+  	  url = url + str(id)
+  	  return url
 
 def get_text():
 	id = '100004087752800'
-#mess = {}
-    txt = ''
-    url = get_text_url(id)
-    soup = get_soup(url)
-    txt = soup.find_all("div", class_ = "qtab-" + str(id))
-    #txt_name = 
-    #mess[txt_name] = txt
-    return txt
+	#mess = {}
+    	txt = ''
+   	url = get_text_url(id)
+   	soup = get_soup(url)
+   	txt = soup.find_all("div", class_ = "qtab-" + str(id))
+  	#txt_name = 
+  	#mess[txt_name] = txt
+  	return txt
 
 def main():
-    mess = get_text()
+   	 mess = get_text()
 	print(mess)
