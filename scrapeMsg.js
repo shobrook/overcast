2650217f7f282dc4fd5d108b32b726d15e3cb13a
Malik <mdrabla1@gmail.com>
2017-01-16 00:52:53 -0500

    pretty

--- scrapeMsg.js
+++ scrapeMsg.js
@@ -2,15 +2,15 @@
 
 containerNode.childNodes.forEach(function(child) { //console.log(child);
     if (child.tagName == 'DIV' && child.id.length > 0) {
-        child.childNodes.forEach(function(c) { //console.log(c);
+    	child.childNodes.forEach(function(c) { //console.log(c);
         	if (c.tagName == 'DIV') {
-                var msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix'); 
-                console.log(msgWrapperNodes); console.log(msgWrapperNodes.length);
-				for (var i = 0; i < msgWrapperNodes.length; i++) {
-					var msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0]; console.log(msgNode.innerHTML);
-					msgNode.innerHTML = 'suck my bussy';
-		      }
-        }
-      });
+                	var msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix'); 
+                	console.log(msgWrapperNodes); console.log(msgWrapperNodes.length);
+			for (var i = 0; i < msgWrapperNodes.length; i++) {
+				var msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0]; console.log(msgNode.innerHTML);
+				msgNode.innerHTML = 'suck my bussy';
+		      	}
+        	}
+      	});
     }
-  });
+});
