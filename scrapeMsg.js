0087e362291e601619c91ad7cca4a48485d13891
Malik <mdrabla1@gmail.com>
2017-01-18 00:13:02 -0500

    Update scrapeMsg.js

--- scrapeMsg.js
+++ scrapeMsg.js
@@ -1,31 +1,42 @@
-//get text elements in main body
-
-/*init  var containerNode = document.getElementsByClassName('__i_')[0];
-
-containerNode.childNodes.forEach(function(child) { //console.log(child);
-    if (child.tagName == 'DIV' && child.id.length > 0) {
-    	child.childNodes.forEach(function(c) { //console.log(c);
-        	if (c.tagName == 'DIV') {
-                	var msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix'); 
-                	console.log(msgWrapperNodes); console.log(msgWrapperNodes.length);
-			for (var i = 0; i < msgWrapperNodes.length; i++) {
-				var msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0]; console.log(msgNode.innerHTML);
-				msgNode.innerHTML = 'suck my bussy';
-		      	}
-        	}
-      	});
-    }
+//find and replace messages
+var findMessages = function() {
+	var containerNode = document.getElementsByClassName('__i_')[0];
+	containerNode.childNodes.forEach(function(child) {
+		if (child.tagName == 'DIV' && child.id.length > 0) {
+			child.childNodes.forEach(function(c) {
+				if (c.tagName == 'DIV') {
+					var msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix'); // Alternatives: ('_41ud')[0].gEBCN('clearfix')
+					for (var i = 0; i < msgWrapperNodes.length; i++) {
+						var msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0]; 
+						if (msgNode === undefined || msgNode === null) {
+							continue; 
+						}
+						msgNode.innerHTML = 'decrypted'; // Need a decrypt() method
+					}
+				}
+			});
+		}
+	});
+}
+var target = document.getElementsByClassName('__i_')[0];
+var config = {attributes: true, subtree: true};
+var msgObserver = new MutationObserver(function(muts) { //find&replace new messages upon update
+	muts.forEach(function(mut) {
+		if (mut.attributeName == 'id' || mut.attributeName == 'class') {
+			console.log('User has scrolled.');
+			findMessages();
+		}
+	});
 });
-*/
-//below doesnt support emojees
-var containerNode = document.getElementsByClassName('_3oh-\ _58nk');
-for(i = 0; i < containerNode.length; i++) {
-	containerNode[i].innerHTML = 'bussy';
-}
 
-//get text elements on the side
-var sideMaster = document.getElementsByClassName('_1htf'); 
-for(i = 0; i < sideMaster.length; i++) {
-	sideMaster[i].innerHTML = 'sex';
-}
+msgObserver.observe(target, config);
 
+//listen on textfield
+document.onkeyup = function(e) { 
+	var key = e.which || e.keyCode; if (key == 13) { 
+		console.log('Enter pressed.'); 
+		} else { 
+			var text = document.activeElement.textContent
+			console.log(text); 
+		} 
+	}
