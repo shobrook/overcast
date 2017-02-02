f5257f81a206b3c538f7659aeac95b4689a47bc0
Jonathan Shobrook <shobrookj@gmail.com>
2017-02-01 23:53:41 -0600

    Layout for login controller

--- app/src/windows/login/login.controller.js
+++ app/src/windows/login/login.controller.js
@@ -1,6 +1,88 @@
-window.onload = function() {
-	function validateForm() {
+// GLOBALS
+
+var {ipcRenderer, remote} = require('electron');
+var app = remote.require('./app.js');
+var Horseman = require('node-horseman');
+
+// MAIN
+
+onload = function() {
+	var horseman = new Horseman(); // Initialize headless browser
+
+	// Listen for a form submission
+	document.forms['form'].onsubmit = function() {
 		var un = document.loginform.usr.value;
 		var pw = document.loginform.pword.value;
+		
+		// Post credentials to fb.com/login and scrape FBID
+		horseman
+		  .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+		  .open('http://www.facebook.com/login.php')
+		  .type('input[id="email"]', un)
+		  .type('input[id="pass"]', pw)
+		  .click('[id="loginbutton"]')
+		  // Return login status, send to main process w/ ipcRenderer
+		  // Wait for page to load(?)
+		  .open('https://www.producthunt.com/')
+		  // missing onload?
+		  .evaluate(function() {
+		  	var fbid = 0;
+
+		  	window.fbAsyncInit = function() {
+		  		FB.init({
+		  			appId: '196762700753455',
+		  			status: true,
+		  			xfbml: true,
+		  			version: 'v2.5'
+		  		});
+		  		FB.getLoginStatus(function(response) {
+		  			if (response.status == 'connected') {
+		  				console.log('We are connected.');
+		  				function getInfo() {
+		  					FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
+		  						if (response.id) {
+		  							fbid = response.id;
+		  							console.log(response.first_name + ' ' + response.last_name);
+		  							console.log(fbid);
+		  						}
+		  					});
+		  				}
+		  				getInfo();
+		  			} else if (response.status == 'not_authorized') {
+		  				console.log('We are not logged in.');
+		  			} else {
+		  				console.log('You are not logged into Facebook.');
+		  			}
+		  		});
+		  	}
+
+		  	(function(d, s, id) {
+		  		var js, fjs = d.getElementsByTagName(s)[0];
+		  		if (d.getElementById(id)) {
+		  			return;
+		  		}
+		  		js = d.createElement(s);
+		  		js.id = id;
+		  		js.src = "https://connect.facebook.net/en_US/sdk.js";
+		  		fjs.parentNode.insertBefore(js, fjs);
+		  	}(document, 'script', 'facebook-jssdk'));
+
+		  	return fbid;
+		  })
+		  .then(function(fbid) {
+		  	console.log(fbid);
+		  })
+		  .log()
+		  .close();
 	}
 }
+
+/* PSEUDO
+ * 1) Initialize headless browser onload of the login page
+ * 2) Scrape user's inputted FB credentials on form submit
+ * 3) Post credentials to fb.com/login in headless browser
+ *      1) On successful login, initialize FBID scraper
+ 			1) Send FBID to main process using ipcRenderer
+ *		2) Else return 'Incorrect email or password.' error screen (new page?)
+ * 4) Initialize loading screen upon receipt of FBID
+*/
