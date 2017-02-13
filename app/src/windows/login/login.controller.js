72c95c955c2c3cd3ca34f0635d6fc036ab4ceb93
Jonathan Shobrook <shobrookj@gmail.com>
2017-02-13 03:42:02 -0600

    Dynamically Δs heading

--- app/src/windows/login/login.controller.js
+++ app/src/windows/login/login.controller.js
@@ -6,16 +6,20 @@ var app = remote.require('./app.js');
 // MAIN
 
 onload = function() {
+	// TO-DO: Prevent form reset onsubmit
+
 	document.forms['form'].onsubmit = function() {
 		var un = document.loginform.usr.value;
 		var pw = document.loginform.pword.value;
 
 		ipcRenderer.send('credentials', {username: un, password: pw}); // Send credentials to main process
 	}
-	
+
 	ipcRenderer.on('login-status', (event, arg) => {
 		if (arg == false) {
-			// TO-DO: Change heading to 'Incorrect email or password.', font-color to #FF2619, and reset form
+			document.getElementById("heading").innerHTML = "Incorrect email or password.";
+			document.getElementById("heading").style["color"] = "#FF2619";
+			// TO-DO: Reset form
 		}
 	});
 }
